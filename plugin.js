e944dff414b3ee439bae3a41ddca65efb410e3f2
Maxmudjon <bio42@mail.ru>
2023-02-05 02:34:01 +0500

    refactoring code and added rgb color light

--- plugin.js
+++ plugin.js
@@ -1,96 +1,87 @@
 "use strict";
 
-var AliceLocal = require('./lib/aliceLocal.js').aliceLocal;
-var aliceActions = require('./lib/aliceActions.js');
-var EventEmitter = require('events').EventEmitter;
-// var debug = require('debug')('alicePlugin');
+const AliceLocal = require("./lib/AliceLocal.js");
+const AliceActions = require("./lib/AliceActions.js");
+const debug = require("debug")("alicePlugin");
+const packageConfig = require("./package.json");
 
-const packageConfig = require('./package.json');
+class AliceHome {
+  constructor(log, config, api) {
+    this.log = log;
+    this.config = config;
+    this.pin = config["pin"] || "031-45-154";
+    this.username = config["username"] || false;
+    this.password = config["password"] || false;
+    this.aliceActions = null;
+    this.aliceLocal = null;
 
-var options = {};
+    // Enable config based DEBUG logging enable
+    this.debug = config["debug"] || false;
+    if (this.debug) {
+      let debugEnable = require("debug");
+      let namespaces = debugEnable.disable();
 
-module.exports = function(homebridge) {
-  homebridge.registerPlatform("homebridge-g-on-alice", "G-On Alice", aliceHome);
-};
-
-function aliceHome(log, config, api) {
-  this.log = log;
-  this.eventBus = new EventEmitter();
-  this.config = config;
-  this.pin = config['pin'] || "031-45-154";
-  this.username = config['username'] || false;
-  this.password = config['password'] || false;
-
-  // Enable config based DEBUG logging enable
-  this.debug = config['debug'] || false;
-  if (this.debug) {
-    let debugEnable = require('debug');
-    let namespaces = debugEnable.disable();
-
-    // this.log("DEBUG-1", namespaces);
-    if (namespaces) {
-      namespaces = namespaces + ',g-on-alice*';
-    } else {
-      namespaces = 'g-on-alice*';
+      // this.log("DEBUG-1", namespaces);
+      if (namespaces) {
+        namespaces = namespaces + ",g-on-alice*";
+      } else {
+        namespaces = "g-on-alice*";
+      }
+      // this.log("DEBUG-2", namespaces);
+      debugEnable.enable(namespaces);
     }
-    // this.log("DEBUG-2", namespaces);
-    debugEnable.enable(namespaces);
+
+    if (!this.username || !this.password) {
+      this.log.error("Missing username and password");
+    }
+
+    if (api) {
+      this.api = api;
+      this.api.on("didFinishLaunching", this.didFinishLaunching.bind(this));
+    }
+
+    this.log.info("%s v%s, node %s, homebridge v%s", packageConfig.name, packageConfig.version, process.version, api.serverVersion);
   }
 
-  if (!this.username || !this.password) {
-    this.log.error("Missing username and password");
+  didFinishLaunching() {
+    const host = "homebridge.g-on.io";
+
+    const options = {
+      username: this.username,
+      password: this.password,
+      clientId: this.username,
+      debug: this.debug,
+      log: this.log,
+      pin: this.pin,
+      servers: [
+        {
+          protocol: "mqtt",
+          host: host,
+          port: 1883,
+        },
+      ],
+    };
+
+    // Initialize HAP Connections
+    this.aliceActions = new AliceActions(options);
+
+    this.aliceLocal = new AliceLocal(options);
+
+    // Alice messages
+    this.aliceLocal.on("discovery", this.aliceActions.aliceDiscovery.bind(this));
+    this.aliceLocal.on("action", this.aliceActions.aliceAction.bind(this));
+    this.aliceLocal.on("query", this.aliceActions.aliceQuery.bind(this));
   }
 
-  if (api) {
-    this.api = api;
-    this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
+  accessories() {
+    return;
   }
 
-  this.log.info(
-    '%s v%s, node %s, homebridge v%s',
-    packageConfig.name, packageConfig.version, process.version, api.serverVersion
-  );
+  configureAccessory() {
+    //
+  }
 }
 
-aliceHome.prototype = {
-  accessories: function(callback) {
-    this.log("accessories");
-    callback();
-  }
-};
-
-aliceHome.prototype.didFinishLaunching = function() {
-  var host = 'homebridge.g-on.io';
-  // var host = 'localhost';
-
-  options = {
-    eventBus: this.eventBus,
-    username: this.username,
-    password: this.password,
-    clientId: this.username,
-    debug: this.debug,
-    log: this.log,
-    pin: this.pin,
-    servers: [{
-      protocol: 'mqtt',
-      host: host,
-      port: 1883
-    }]
-  };
-
-  // Initialize HAP Connections
-  aliceActions.hapDiscovery(options);
-
-  var alice = new AliceLocal(options);
-
-  // Alice mesages
-
-  this.eventBus.on('discovery', aliceActions.aliceDiscovery.bind(this));
-  this.eventBus.on('action', aliceActions.aliceAction.bind(this));
-  this.eventBus.on('query', aliceActions.aliceQuery.bind(this));
-};
-
-aliceHome.prototype.configureAccessory = function(accessory) {
-  this.log("configureAccessory");
-  // callback();
+module.exports = (homebridge) => {
+  homebridge.registerPlatform("homebridge-g-on-alice", "G-On Alice", AliceHome);
 };
